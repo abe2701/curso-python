6a827eb0284e5a60e45e2b68fe458fd35476229b
abe2701 <abe2701@live.com>
2021-12-29 19:07:51 -0300

    pasta logica de programacao

---
+++ Básico(Lógica de programação)/1.comentarios.py
@@ -0,0 +1,6 @@
+# Isso é um comentário
+print('1')
+print('2')
+print('3')
+"""print('4')
+print('5')"""
---
+++ Básico(Lógica de programação)/2.comando-print.py
@@ -0,0 +1,3 @@
+print('Abel','Maria','João',sep='-',end='#\n')
+print('824','176','070',sep='.',end='-')
+print('18')
